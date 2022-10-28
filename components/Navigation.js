978f45e88542d6d007947ff52d540366737e0c35
Chakit Arora <chakit@192.168.1.8>
2022-10-28 17:01:20 +0530

    fix-nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -71,6 +71,9 @@ const Navigation = () => {
           </div>
         </div>
       </div>
+          {/* <!--
+        Mobile menu, show/hide based on mobile menu state.
+      --> */}
       {openMenu && (
         <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
